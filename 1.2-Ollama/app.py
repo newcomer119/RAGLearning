3ba8ca2842a3328b2cf39764358bce0c1c71590f
newcomer119 <mitarth.125035@stu.upes.ac.in>
2026-02-08 21:35:36 +0530

    made changes

---
+++ 1.2-Ollama/app.py
@@ -0,0 +1,36 @@
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers.string import StrOutputParser
+
+## LANGSMITH TRACKING
+
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
+
+## Prompt Template 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the question asked "),
+        ("user","Question:{question}")
+    ]
+)
+
+## STREAM LIT FRAMEWORK
+st.title("LANGCHAIN Demo With LLAMA2")
+input_text = st.text_input("What Question do you have in your mind")
+
+## Ollama LLama model
+llm = Ollama(model="gemma:2b")
+output_parser =StrOutputParser()
+chain = prompt|llm|output_parser
+
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
